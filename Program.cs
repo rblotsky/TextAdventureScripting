511d3148c3865f356c5a8eeeb619354d2165cfe1
rblotsky <roman.blotsky@gmail.com>
2022-07-09 15:15:38 -0400

    Add initial features

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextAdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColourConsole.WriteLine("Hello World!", ConsoleColor.Green);
+        }
+    }
+}
